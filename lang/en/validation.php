ef695fe645eef07961af63af5c6ab2994cc1af5e
Roberto Gallea <write@robertogallea.com>
2022-08-30 12:42:47 +0200

    Apply fixes from StyleCI (#49)
    * Apply fixes from StyleCI
    
    * Fix styling
    
    Co-authored-by: StyleCI Bot <bot@styleci.io>
    Co-authored-by: robertogallea <robertogallea@users.noreply.github.com>

--- lang/en/validation.php
+++ lang/en/validation.php
@@ -1,11 +1,11 @@
 <?php
 
 return [
-    'wrong_size' => 'The :attribute has a wrong size',
-    'no_code' => 'The :attribute is empty',
-    'bad_characters' => 'The :attribute contains bad characters',
+    'wrong_size'        => 'The :attribute has a wrong size',
+    'no_code'           => 'The :attribute is empty',
+    'bad_characters'    => 'The :attribute contains bad characters',
     'bad_omocodia_char' => 'The :attribute contains bad omocodia characters',
-    'wrong_code' => 'The :attribute is not valid',
+    'wrong_code'        => 'The :attribute is not valid',
     'missing_city_code' => 'The :attribute contains a non-existing city code',
-    'no_match' => 'The :attribute does not match the given personal information',
+    'no_match'          => 'The :attribute does not match the given personal information',
 ];
